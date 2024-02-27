dfbe26fe70def482bad9397ece4849f1f732902c
godafflig <tombenat99@hotmail.fr>
2024-02-27 16:22:31 +0100

    ajout de la fonction de register stable avec message error

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -9,6 +9,8 @@ require_once __DIR__ . '/../Modele/RegisterModel.php';
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
+use function PHPSTORM_META\type;
+
 class RegisterController
 {
     protected $twig;
@@ -21,7 +23,6 @@ class RegisterController
         $this->loader = new FilesystemLoader(__DIR__ . '/../Vue/Template');
         $this->twig = new Environment($this->loader);
         $this->registerModel = new RegisterModel();
-
 
 
         $context = [];
@@ -37,13 +38,28 @@ class RegisterController
             // Vous pouvez ajouter une validation supplémentaire ici
             // Envoi de la demande d'enregistrement
             $response = $this->registerModel->register($email, $displayname, $password, $username);
+            // var_dump($response);
+            if ($response->code == 200) {
+                $context = ['message' => "success", 'type' => "success"];
+            } else if ($response->code == 400 && isset($response->errorDetails)) { {
+                    $errors = [];
+                    foreach ($response->errorDetails as $field => $messages) {
+                        foreach ($messages as $message) {
+                            $errors[] = $message; // Ajoute chaque message d'erreur à un tableau
+                        }
+                    }
+                    $context['errors'] = $errors; // Ajoute le tableau d'erreurs au contexte pour Twig
+                }
+            } else {
+                $context = ['message' => $response->errorMessage, 'type' => "error"];
+            }
+
             // Traiter la réponse et effectuer des actions en fonction de celle-ci
             // Par exemple, afficher un message ou rediriger l'utilisateur
 
-            print_r($response);
 
             //REDIRECTION EN FONCTION DE LA REPONSE->
         }
-        $this->twig->display('Register/register.html.twig', []);
+        $this->twig->display('Register/register.html.twig', $context);
     }
 }
